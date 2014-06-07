ac0f2985181b1454e4e66999a5d59af8d6023c19
mgmcnally723 <mgmcnally@umass.edu>
2014-06-06 23:39:06 -0400

    made the enemies to update as well

--- Update.elm
+++ Update.elm
@@ -6,11 +6,20 @@ import GameState (..)
 import Asteroid
 import Ship (Ship)
 import Ship
+import Enemy
 
-update : RealWorld -> Input -> GameState -> GameState
+-- update : RealWorld -> Input -> GameState -> GameState
 update realWorld input state =
+   -- let  numEnemies = state.enemies.length
+   --     xs = repeat 5 ship'.x
+   --     ys = repeat 5 ship'.y
     let ship' = Ship.update input state.ship
         asteroids' = Asteroid.updateAll state.asteroids
+        updateEnemies ship = { ship | vx <- state.ship.x, vy <- state.ship.y }
+        enemies' = Enemy.updateAll (map updateEnemies state.enemies) -- ship'.x state.ship.y --
     in case input of
-         otherwise -> {state | ship <- ship', asteroids <- asteroids' }
+         otherwise -> {state | ship <- ship',
+                               asteroids <- asteroids',
+                               enemies <- enemies' }
+
 

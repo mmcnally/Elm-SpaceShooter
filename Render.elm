4589e9032803aa930f4d2125ced20f38d04b0e74
mgmcnally723 <mgmcnally@umass.edu>
2014-06-22 22:38:56 -0400

    deleted function that wasn't used

--- Render.elm
+++ Render.elm
@@ -6,14 +6,11 @@ import Asteroid
 import Enemy
 
 
--- note: fixAngle function never seems to be called and game functions
---       normally when it is commented out
 render : GameState -> [Form]
 render state = 
     let shipForm = Ship.render state.ship
         asteroidForms = map Asteroid.render state.asteroids
         enemyForms = (map Enemy.render) state.enemies
         forms = enemyForms ++ asteroidForms ++ [shipForm]
-        fixAngle = -state.ship.angle + pi/2
         fixPosition = (-state.ship.x, -state.ship.y)
     in  map (move  fixPosition) forms

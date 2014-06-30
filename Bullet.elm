396516f81cfe385359a258aae8dfc94bd90f5712
mgmcnally723 <mgmcnally@umass.edu>
2014-06-30 16:37:18 -0400

    old bullets delete to reduce game lag

--- Bullet.elm
+++ Bullet.elm
@@ -32,24 +32,40 @@ bullet x y vx vy speed size angle birthtime = { x = x,
                                             angle = angle,
                                         birthtime = birthtime }
 
+-- applies physics to a bullet
 physics : Bullet -> Bullet
 physics b = if |  b.birthtime > 200 -> {b | size <- 0}
                |        otherwise -> { b | x <- b.x + (b.vx * b.speed),
                                            y <- b.y + (b.vy * b.speed),
                                    birthtime <- b.birthtime + 1}
+
+-- applied physics to each bullet in list
 updateAll: [Bullet] -> [Bullet]
 updateAll = map physics
 
 
+-- determines whether a bullet is young enough to be kept
+-- predicate in filter function in checkBulletTime
+youngEnough: Bullet -> Bool
+youngEnough bullet = bullet.birthtime < 100
+
+-- filters a list of bullets based on birthtime
+-- eliminates old bullets
+checkBulletTime: [Bullet] -> [Bullet]
+checkBulletTime bullets = filter youngEnough bullets
+
+-- updates all bullets
 update : Input -> [Bullet] -> Ship {} -> [Bullet]
 update input bullets ship = 
-    case input of
+    let bullets' = checkBulletTime bullets
+    in case input of
       Key key ->
           if | key `Keys.equals` Keys.space ->
-                                   addBullet bullets ship
-             | otherwise -> bullets
-      Passive t -> updateAll bullets
-      otherwise -> bullets
+                                   addBullet bullets' ship
+             | otherwise -> bullets'
+      Passive t -> updateAll bullets'
+      otherwise -> bullets'
+
 
 render : Bullet -> Form
 render {x, y, size} = circle size |> filled green |> move (x,y)

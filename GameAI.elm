91ae2768639a61a3fd4f0f2c9082a622afd27ec9
mgmcnally723 <mgmcnally@umass.edu>
2014-07-11 21:22:53 -0400

    moved enemy functions to Enemy.elm, added several random generation functions

--- GameAI.elm
+++ GameAI.elm
@@ -1,29 +1,33 @@
 module GameAI where
-import Ship (..)
-import Enemy (..)
-import GameState (..)
 import Generator (..)
 import Generator.Standard (..)
 
 
--- creates a new enemy 
-updateState: GameState -> GameState
-updateState state = 
-  { state | enemies <- addEnemy state }
 
--- adds one enemy with random speed
-addEnemy: GameState -> [EnemyShip {}]
-addEnemy state = if (length state.enemies) < 20 
-                        then { enemy | x <- (head state.enemies).x * 1.5,
-                                       y <- (head state.enemies).y + 100,
-                                       speed <- randomNum state.ship, 
-                                       intel <- 50 }::state.enemies
-                        else state.enemies
+-- will create a random number between
+-- given range with given seed
+randomNum: Float -> Float -> Float -> Float
+randomNum lower upper seed =
+    fst <| floatRange (lower, upper) (generator <| floor seed)
 
--- generates a random float between 0 and 1
-randomNum: Ship {} -> Float
-randomNum ship = fst <| float <| generator <| seed ship
 
--- creates a somewhat random seed for the generator
-seed: Ship {} -> Int
-seed ship = floor <| (ship.x + 0.7) * 345.0 + (ship.y + 8.4) * 837.0
+-- random number between 0 and 1 based
+-- on given seed
+randomFloat seed = randomNum (0) (1) (seed)
+
+
+
+-- generates starting x-coordinate
+-- off left side of the screen
+xLeft shipX seed = randomNum (shipX - 900) (shipX - 700) seed
+
+
+-- generates starting x-coordinate
+-- off right side of screen
+xRight shipX seed = randomNum (shipX + 700) (shipX + 900) seed
+
+-- generates starting y-coordinate above screen
+yUp shipY seed = randomNum (shipY - 900) (shipY - 700) seed
+
+-- generates starting y-coordinate below screen
+yDown shipY seed = randomNum (shipY + 700) (shipY + 900) seed

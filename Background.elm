1501e12504d5129889fbe808e4b3e46592217beb
mgmcnally723 <mgmcnally@umass.edu>
2014-07-14 16:36:28 -0400

    stars spawn slightly better, background color now black

--- Background.elm
+++ Background.elm
@@ -13,24 +13,23 @@ initialStar = { x = 0,
                 color = yellow,
                 size = 10 }
 
-
-
-
 -- makes some stars
 addStars: [Star] -> Ship {} -> Float -> [Star]
 addStars stars ship time = 
-    if (length stars) < 50
+    if (length stars) < 100 || ((floor time) `mod` 2) == 0
     then (createStar ship time)::stars
     else stars
-    
+
+
+-- creates a star   
 createStar: Ship {} -> Float -> Star
 createStar ship time = 
     let numX = randomFloat time
         numY = randomFloat (time * 3.54)
-        xL = xLeft ship.x (time * 1.54)
-        xR = xRight ship.x (time * 2.73)
-        yU = yUp ship.y (time * 9.05)
-        yD = yDown ship.y (time * 4.63)
+        xL = randomNum (ship.x - 1500) (ship.x - 700) (time * 1.54)
+        xR = randomNum (ship.x + 700) (ship.x + 1500) (time * 2.73)
+        yU = randomNum (ship.y - 1500) (ship.y - 700)(time * 9.05)
+        yD = randomNum (ship.y + 700) (ship.y + 1500) (time * 4.63)
         x = if numX < 0.5 
             then xL 
             else xR
@@ -41,15 +40,33 @@ createStar ship time =
                        y <- y }
 
 
+
+--decides if star is too far away
+closeEnough: Star -> Ship {} -> Bool
+closeEnough star ship =
+    if (abs (ship.x - star.x)) < 1200 && (abs (ship.y - star.y)) < 1500
+    then True
+    else False
+
+
+-- deletes stars that are too far away
+deleteStars: [Star] -> Ship {} -> [Star]
+deleteStars stars ship = filter (flip closeEnough ship) stars
+
+
+
+
+-- checks to see if there should be more
+-- stars and creates a star if needed
 update: [Star] -> Ship {} -> Float -> [Star]
 update stars ship time = 
     let stars' = addStars stars ship time
-    in stars'
-
+        stars'' = deleteStars stars' ship
+    in stars''
 
 -- renders single color background
 renderBackground: Form
-renderBackground = filled gray (square 1500)
+renderBackground = filled black (square 1500)
 
 --renders stars
 renderStars: Star -> Form

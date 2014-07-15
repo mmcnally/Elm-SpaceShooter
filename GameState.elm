c9c8de1bc94086ebdb3e686b700a28afc8b6c544
mgmcnally723 <mgmcnally@umass.edu>
2014-07-14 20:43:18 -0400

    fixed compilation error

--- GameState.elm
+++ GameState.elm
@@ -19,7 +19,7 @@ initialState = { ship = initialShip,
                  enemies = [ {enemy | x <- 50, y <- 50, speed <- 1.0, playerX <- 50},
                              {enemy | x <- -100, y <- 200, speed <- 0.4 },
                              {enemy | x <- 100, y <- -150, speed <- 0.1 }],
-                 bullets = [bullet 200 200 0 0 0 0 0 0],
+                 bullets = [bullet 200 200 0 0 0 0 0 0 (0, 0)],
                  time = 0,
                  frameRate = 0.0,
                  stars = [initialStar] }

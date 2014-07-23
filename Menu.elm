0fac4c399082de05a834b38ffef4bf30c5084102
mgmcnally723 <mgmcnally@umass.edu>
2014-07-22 21:22:37 -0400

    improved button clicking

--- Menu.elm
+++ Menu.elm
@@ -9,18 +9,19 @@ import Playground.Input (..)
 render: [Form]
 render = 
     let background = [filled blue <| rect 300 400]
-        text = [move (0, 150) <| 
+        startTextBackground = [move (0, 150) <| filled darkBlue <| rect 200 50]
+        startText = [move (0, 150) <| 
                        toForm <| 
                      centered <| 
                      height 40 (toText "Start Game") ]
-    in background ++ text
+    in background ++ startTextBackground ++ startText
 
 update: RealWorld -> Input -> GameState -> GameState
 update realWorld input state  = 
     let x = realWorld.mouse.x
         y = realWorld.mouse.y
     in case input of 
-      Click -> if | (x < 100 && x > -100 && y < 170 && y > 125) -> 
+      Click -> if | (x < 100 && x > -100 && y < 175 && y > 125) -> 
                                             { state | isPlaying <- True }
                   | otherwise -> state
       otherwise -> state

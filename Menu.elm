e8a0aed9724ba73bb519f343295916cfc73ba4ee
mgmcnally723 <mgmcnally@umass.edu>
2014-07-29 18:26:31 -0400

    fixed cursor changing issue

--- Menu.elm
+++ Menu.elm
@@ -5,20 +5,35 @@ import GameState (..)
 import Playground (..)
 import Playground.Input (..)
 import Keyboard.Keys as Keys
+import Graphics.Input
+
+click : Graphics.Input.Input ()
+click = Graphics.Input.input ()
+
+prettyButton : Element
+prettyButton =
+      Graphics.Input.customButton click.handle ()
+          (centered <| height 40 (toText "Start Game"))
+          (centered <| height 40 (toText "Start Game"))
+          (centered <| height 40 (toText "Start Game"))
+
 
 render: GameState -> [Form]
 render state = 
     let menuBackground = [filled darkPurple <| rect 300 400]
         num = snd state.isPlaying
-        color = if num == 0 then darkOrange else purple
+        color = if num == 0 then darkOrange else lightPurple
+        buttonBackground = [ move (0, 150) <| filled color <| rect 200 50]
         background = [filled black <| rect 1500 1500]
-        startTextBackground = [move (0, 150) <| filled color <| rect 200 50]
-        startText = [move (0, 150) <| 
-                            toForm <| 
-                          centered <| 
-                     height 40 (toText "Start Game") ]
+        button = [ move (0, 150) (toForm prettyButton) ]
+        startTextCover = [move (0, 150) <| 
+                          outlined (solid darkPurple) (rect 300 100)]
        
-    in background ++ menuBackground ++ startTextBackground ++ startText
+    in background ++ 
+       menuBackground ++ 
+       buttonBackground ++
+       button ++
+       startTextCover
 
 update: RealWorld -> Input -> GameState -> GameState
 update realWorld input state  = 
